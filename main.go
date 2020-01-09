ce5455713619598e3e8ce95826a2729435dde0bb
utkarsh <srivastavautkarsh8097@gmail.com>
2020-01-09 20:09:33 +0530

    Added json decoder

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// JSON struct describes the json payload
+type JSON struct {
+	Language string            `json:"language"`
+	Command  string            `json:"command"`
+	Files    []EvaluationFiles `json:"files"`
+	Stdin    string            `json:"stdin"`
+}
+
+// EvaluationFiles hold the structure for the files that should be evaluated
+type EvaluationFiles struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+// Result struct
+type Result struct {
+	Stdout string `json:"stdout"`
+	Stderr string `json:"stderr"`
+}
+
+func main() {
+	decodedJSON := &JSON{}
+	err := json.NewDecoder(os.Stdin).Decode(decodedJSON)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("Type: %T\nContent: %+v\nAddress: %p\n", *decodedJSON, *decodedJSON, decodedJSON)
+}
